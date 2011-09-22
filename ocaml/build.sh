24024b5d95dc607a048fbf1e2dd59c6ba7353c75
Pat Rondon <prondon@cs.ucsd.edu>
2011-09-21 21:29:43 -0700

    also build z3 byte objects a la ruslan

--- ocaml/build.sh
+++ ocaml/build.sh
@@ -8,3 +8,7 @@ ocamlopt -c z3.ml
 ar rcs libz3stubs.a z3_stubs.o z3_theory_stubs.o
 ranlib libz3stubs.a
 ocamlopt -a -o z3.cmxa -cclib -lz3stubs z3.cmx
+
+ocamlc -c z3.mli
+ocamlc -c z3.ml
+ocamlc -a -o z3.cma -cclib -lz3stubs z3.cmo
